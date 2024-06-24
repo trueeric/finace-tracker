a19575ca2a8387e5e445de98fb6dfc868213a09c
Eric Uen <ikpassat@gmail.com>
2024-06-24 10:28:53 +0800

    059_Summary Section and Period Selection

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
-	<div>Index Page!</div>
+	<section class="flex items-center justify-between mb-10">
+		<h1 class="text-4xl font-extrabold">Summary</h1>
+		<USelectMenu :options="transactionViewOptions" v-model="selectedView" />
+	</section>
 </template>
 
-<script setup></script>
+<script setup>
+import { transactionViewOptions } from '~/constants'
+const selectedView = ref(transactionViewOptions[1])
+</script>
 
 <style lang="scss" scoped></style>
