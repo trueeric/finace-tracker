00665e30039e9f58241355f188b163ceb649529c
Eric Uen <ikpassat@gmail.com>
2024-06-24 17:49:28 +0800

    061_Trend Component (Showing Percentage Difference)

--- components/trend.vue
+++ components/trend.vue
@@ -9,20 +9,44 @@
 		<div>
 			<USkeleton class="h-6 w-full" v-if="loading" />
 			<div class="flex space-x-1 items-center text-sm" v-else>
-				<UIcon name="i-heroicons-arrow-trending-up" class="w-6 h-6" :class="[color]" />
-				<div class="text-gray-500 dark:text-gray-400">30% vs last period</div>
+				<UIcon :name="icon" class="w-6 h-6" :class="{ green: trendingUp, red: !trendingUp }" />
+				<div class="text-gray-500 dark:text-gray-400">{{ percentageTrend }} vs last period</div>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script setup>
-defineProps({
+const props = defineProps({
 	title: String,
 	amount: Number,
 	lastAmount: Number,
 	color: String,
 	loading: Boolean,
+})
+
+const trendingUp = computed(() => props.amount >= props.lastAmount)
+const icon = computed(() => (trendingUp.value ? 'i-heroicons-arrow-trending-up' : 'i-heroicons-arrow-trending-down'))
+
+// const percentageTrend = computed(() => {
+// 	if (props.amount === 0 || props.lastAmount === 0) return '0'
+
+// 	const bigger = Math.max(props.amount, props.lastAmount)
+// 	const lower = Math.min(props.amount, props.lastAmount)
+
+// 	const ratio = ((bigger - lower) / lower) * 100
+// 	console.log(bigger, lower, ratio, Math.ceil(ratio))
+// 	return ratio
+// })
+
+const percentageTrend = computed(() => {
+	if (props.amount === 0 || props.lastAmount === 0) return '0'
+
+	const difference = props.amount - props.lastAmount
+	const ratio = (difference / props.lastAmount) * 100
+	// console.log('ratio', Math.ceil(ratio))
+
+	return `${Math.round(ratio * 10) / 10}%`
 })
 </script>
 
@@ -30,6 +54,7 @@ defineProps({
 .green {
 	@apply text-green-600 dark:text-green-400;
 }
+
 .red {
 	@apply text-red-600 dark:text-red-400;
 }
