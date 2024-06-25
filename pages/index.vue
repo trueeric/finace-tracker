4ba35da0246f8d42420f987fe3d41d798e55a588
Eric Uen <ikpassat@gmail.com>
2024-06-25 17:43:28 +0800

    070_Daily Summary of Transactions

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,10 @@
 		<Trend color="red" title="Saving" :amount="4000" :last-amount="4700" :loading="false" />
 	</section>
 	<section>
-		<Transaction v-for="transaction in transactions" :key="transaction.id" :transaction="transaction" />
+		<div v-for="(transactionsOnDay, date) in transactionsGroupedByDate" :key="date" class="mb-10">
+			<DailyTransactionSummary :date="date" :transactions="transactionsOnDay" />
+			<Transaction v-for="transaction in transactionsOnDay" :key="transaction.id" :transaction="transaction" />
+		</div>
 	</section>
 </template>
 
