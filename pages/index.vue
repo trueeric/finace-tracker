47f71fd66e36ab6e67b4f0ed4cac35674b39b09f
Eric Uen <ikpassat@gmail.com>
2024-07-14 22:00:22 +0800

    104_User Profile: Storing and Using App User Preferences

--- pages/index.vue
+++ pages/index.vue
@@ -39,7 +39,10 @@
 
 <script setup>
 import { transactionViewOptions } from '~/constants'
-const selectedView = ref(transactionViewOptions[1])
+
+const user = useSupabaseUser()
+
+const selectedView = ref(user.value.user_metadata?.transaction_view ?? transactionViewOptions[1])
 
 const isOpen = ref(false)
 
--- pages/settings/settings.vue
+++ pages/settings/settings.vue
@@ -1,1 +1,57 @@
-<template>Settings!</template>
+<template>
+	<UForm :state="state" :schema="schema" @submit="saveSettings">
+		<UFormGroup class="mb-4" label="Transaction view" help="Choose how you would like to view">
+			<USelect v-model="state.transactionView" :options="transactionViewOptions" />
+		</UFormGroup>
+
+		<UButton type="submit" color="black" variant="solid" label="Save" :loading="pending" :disabled="pending" />
+	</UForm>
+</template>
+
+<script setup>
+import { z } from 'zod'
+import { transactionViewOptions } from '~/constants'
+
+const supabase = useSupabaseClient()
+const user = useSupabaseUser()
+
+const { toastSuccess, toastError } = useAppToast()
+const pending = ref(false)
+
+const state = ref({
+	// * 先看有沒有值，沒值預設為transactionViewOptions[1](monthly)
+	transactionView: user.value.user_metadata?.transaction_view ?? transactionViewOptions[1],
+})
+
+const schema = z.object({
+	transactionView: z.enum(transactionViewOptions),
+})
+
+const saveSettings = async () => {
+	pending.value = true
+
+	try {
+		const data = {
+			data: {
+				transaction_view: state.value.transactionView,
+			},
+		}
+
+		const { error } = await supabase.auth.updateUser(data)
+		if (error) throw error
+		toastSuccess({
+			title: 'Settings updated.',
+			description: 'Your settings has been updated.',
+		})
+	} catch (error) {
+		toastError({
+			title: 'Error updating profile',
+			description: error.message,
+		})
+	} finally {
+		pending.value = false
+	}
+}
+</script>
+
+<style lang="scss" scoped></style>
